2d3d1917c00ce4b5153256ee5dc20b2b4eefeec9
Olda Krup <djslimelp@gmail.com>
2019-06-03 08:42:04 +0000

    Adding new code

--- 2019/Personal web/server.js
+++ 2019/Personal web/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 
-const request = require("request-promise");
+const requestPromise = require("request-promise");
+const request = require("request");
 const fs = require("fs");
 const path = require("path");
 
@@ -28,12 +29,43 @@ function githubHandler(req, res){
   console.log("Category: " + category);
 
   // When category = to the Web category
-  if(category = categories.Web){
+  if(category == categories.Web){
 
-    var contents = getContentsOfFolder(category);
-    contents.then(function(response){
-      console.log(contents);
-      console.log("YES");
+    var promise = getContentsOfFolder(category);
+    promise.then(function(contents){
+      
+      var temp = [];
+      for(var i = 0; i < contents.length; i++){
+        
+        // Getting the main directories (years)
+        if("20" == contents[i].name.substring(0, 2)){
+          temp.push(contents[i].name); 
+        }
+        
+      }
+      contents = temp;
+      temp = [];
+      
+      // Getting the folders from the years folders
+      for(var i = 0; i < contents.length; i++){
+        
+        var promise = getContentsFromFolder(category, contents[i]);
+        promise.then(function(folders){
+          
+          // For all the folders
+          for(var j = 0; j < folders.length; j++){
+          
+            temp.push(folders[j].name);
+            
+          }
+          
+          contents = temp;
+      
+          
+        });
+         
+      }
+      
     });
 
     // TODO: Web
@@ -41,24 +73,45 @@ function githubHandler(req, res){
   }
 
   res.send("OK");
+
 }
 
+// Get the folders from years directory
+function getContentsFromFolder(category, path){
+  
+  var githubAPI = "https://api.github.com/repos/ItsOKayCZ/" + category + "/contents/" + path;
+  
+  return makeRequest(request, githubAPI);
+}
+
+// Gets the years folders from the root directory
 function getContentsOfFolder(category){
 
   // TODO: Add token to URL
   var githubAPI = "https://api.github.com/repos/ItsOKayCZ/" + category + "/contents";
 
+  return makeRequest(requestPromise, githubAPI);
+}
+
+// Make a request
+// Is isPromise = true
+// Will use request-promise
+function makeRequest(isPromise, URL){
+  
+  // TODO: Grab token from file
+  
   var options = {
-    uri: githubAPI,
+    uri: URL,
     headers: {
       "User-Agent": "Personal web API"
     },
     json: true
   };
+  
+  if(isPromise == true){
+    var promise = new Promise(function(resolve, reject){
 
-  var promise = new Promise(function(resolve, reject){
-
-    request(options)
+    requestPromise(options)
       .then(function(response){
         resolve(response);
       })
@@ -66,9 +119,24 @@ function getContentsOfFolder(category){
         console.log(err);
       });
 
-  });
-
-  return promise;
+    });
+    
+    return promise;  
+  
+  } else {
+    
+    // TODO: without promise
+    
+    // var promise = new Promise(function(resolve, reject){
+      
+    //   request(options, function(err, response, body){
+    //     resolve(response);
+    //   });
+      
+    // });
+    
+  }
+  
 }
 
 // All files
