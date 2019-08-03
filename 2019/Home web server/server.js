1c75a044298c3fe6589bf50722927960890c0bfb
ItsOKayCZ <oldrich.krupauer@gmail.com>
2019-08-03 21:23:56 +0200

    Fix

--- 2019/Home web server/server.js
+++ 2019/Home web server/server.js
@@ -25,7 +25,7 @@ router.use(function(req, res, next){
 
   if(req.url == "/" + cookie){
 
-    res.cookie("auth", cookie, {maxAge: 365 * 10 * 60 * 60 * 1000});
+    res.cookie("auth", cookie, {maxAge: 50 * 365 * 24 * 60 * 60 * 1000});
     console.log("[#] Cookie sent to " + req.ip);
     res.redirect("/");
     return;
